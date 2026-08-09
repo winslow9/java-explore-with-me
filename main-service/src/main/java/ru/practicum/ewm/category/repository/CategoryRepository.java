5d467f9983ea275c8124464f02b26f9628ea9d81
Denis Orlov <des2033@gmail.com>
2026-08-09 13:46:09 +0400

    remove unused imports

--- main-service/src/main/java/ru/practicum/ewm/category/repository/CategoryRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/category/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.category.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.category.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
